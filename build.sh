a585646591bed32333cd86bec1ac9de45732bbbf
linwoain <564804771@qq.com>
2022-11-26 11:57:41 +0800

    Update build.sh
    修复aar中classes.jar转化为dex时未脱糖导致的crash

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 ./gradlew app-compose-ui-devices:build
 cd app-compose-ui-devices/build/outputs/aar
 unzip app-compose-ui-devices-release.aar
-~/Library/Android/sdk/build-tools/30.0.3/dx --dex --min-sdk-version=26 --output=compose_ui_devices.dex classes.jar
-cp compose_ui_devices.dex ../../../../app/src/main/assets
-./gradlew assembleDebug
+~/Library/Android/sdk/build-tools/30.0.3/d8 classes.jar
+cp classes.dex ../../../../app/src/main/assets/compose_ui_devices.dex
+./gradlew installDebug
